22662491efead155a182b5bf5397418939cabac2
juniordenny69 <juniordenny@gmail.com>
2013-08-15 13:44:13 -0400

    sandwich assignment

--- src/Sandwich.java
+++ src/Sandwich.java
@@ -1,16 +1,34 @@
 
 public class Sandwich {
 
-	/* 
-	 * This is the Sandwich class.
-	 * This is an object class.
-	 * There will be no main() method here
-	 * Instead, you will declare variables to create
-	 * the object's "fields", and then add methods 
-	 * to retrieve or change the value of each field.
-	 * 
-	 */
+		//Declare variables for sandwich
+		static String breadType;
+		static String ingredient;
+		static double price;
+	
+		//Returns declarations for sandwich
+		public String getBreadType()	{
+			return breadType;
+		}
+		public String getIngredient()	{
+			return ingredient;
+		}		
+		public double getPrice()	{
+			return price;
+		}
+
+		//Set new values of the above declarations
+		public void setBreadType(String newBreadType)	{
+			breadType = newBreadType;
+		}		
+		public void setIngredient(String newIngredient)	{
+			ingredient = newIngredient;
+		}		
+		public void setPrice(int newPrice)	{
+			price = newPrice;
+		}
+		
+	}
+	
 
 
-
-}
--- src/TestSandwich.java
+++ src/TestSandwich.java
@@ -1,21 +1,34 @@
+import javax.swing.JOptionPane;
+
 
 public class TestSandwich {
 
 	public static void main(String[] args) {
-		/*
-		 * This class is a program class.
-		 * It WILL include a main() method.
-		 * This program should create two instances of
-		 * Sandwich, and it should demonstrate the
-		 * use of all of the methods encapsulated
-		 * in the Sandwich class.
-		 * 
-		 * Finish off this program by displaying the
-		 * values of each of the fields for each 
-		 * Sandwich instance.
-		 * 
-		 */
+		//Instantiate a sandwich object
+		Sandwich JrSandwich = new Sandwich();
+		Sandwich KeithSandwich = new Sandwich();
+		
+		//Set values of fields in JrSandwich
+		JrSandwich.setBreadType(" whole wheat");
+		JrSandwich.setIngredient(" tuna");
+		JrSandwich.setPrice(5);
+		
+		//Output the details of JrSandwich
+		String customer;
+		customer = JOptionPane.showInputDialog("Whose sandwich is this?");
+		JOptionPane.showMessageDialog(null, customer + " is getting a " + JrSandwich.getBreadType() 
+				+" sandwich with" + JrSandwich.getIngredient() + " it will cost " + JrSandwich.getPrice() +" dollars.");
 
+		//Set values of fields in KeithSandwich
+		KeithSandwich.setBreadType(" white");
+		KeithSandwich.setIngredient(" chicken");
+		KeithSandwich.setPrice(7);
+		
+		//Output the details of KeithSandwich
+		String customer1;
+		customer1 = JOptionPane.showInputDialog("Whose sandwich is this?");
+		JOptionPane.showMessageDialog(null, customer1 + " is getting a " + KeithSandwich.getBreadType() 
+				+" sandwich with" + KeithSandwich.getIngredient() + " it will cost " + KeithSandwich.getPrice() +" dollars.");
 	}
 
 }
